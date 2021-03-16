68a3f2baf093785ceab38db485301d3013c8a6d8
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-16 10:31:27 +0100

    Created ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,22 @@
+// Write code below this line
+
+// Write code above this line
+function isLess(a, b){
+    if (a<b){
+        return true;
+// Write code below this line
+
+// Write code above this line        
+    } else{
+// Write code below this line
+
+// Write code above this line        
+        return false;
+    }
+    // Write code below this line
+
+// Write code above this line
+}
+console.log(isLess(10,15));
+console.log(isLess(15,10));
+module.exports = isLess;
