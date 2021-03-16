95bf326daaad9750d51f8a82774487e0c443cb0d
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-16 09:57:15 +0100

    Created ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,18 @@
+var dress = "little dress";
+// Only change code below this line
+
+function myDress(){
+
+// Only change code above this line
+var dress="ball gown";
+
+return dress;
+
+}
+// Only change code below this line
+
+// Only change code above this line
+
+console.log(dress)
+myDress();
+module.exports = myDress;
