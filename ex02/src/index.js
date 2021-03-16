213c39d7e071f11db4b664e276052b7e90d7a03b
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-16 08:47:43 +0100

    Created ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+// Write code below this line
+
+
+
+// Write code above this line
+function functionPrintInfo(name,age){
+    console.log(name,age);
+
+}
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+module.exports = functionPrintInfo;
